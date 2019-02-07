ae1f34f69e233171a46b6e2ace126e47bb0dce92
DSaquin <dawnsaq@gmail.com>
2019-02-07 13:44:53 -0800

    implemented knex query to retrieve sample data from db

--- database-pg/index.js
+++ database-pg/index.js
@@ -4,4 +4,17 @@ var knex = require("knex")(config[env]);
 
 module.exports = knex;
 
+const findMostRecent10 = function() {
+  console.log("accessing postgres db.....");
+  return knex
+    .from("reviews")
+    .limit(10)
+    .then(records => {
+      return records;
+    });
+  //   console.log(queryString);
+};
+
 knex.migrate.latest([config]);
+
+module.exports = { findMostRecent10 };
