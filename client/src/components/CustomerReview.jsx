4f71728159709b309128cb90541879ef50a755a9
DSaquin <dawnsaq@gmail.com>
2019-02-07 15:59:59 -0800

    created axios get request to server

--- client/src/components/CustomerReview.jsx
+++ client/src/components/CustomerReview.jsx
@@ -3,8 +3,8 @@ import moment from "moment";
 
 const CustomerReview = props => (
   <li className="">
-    <img src={props.review.image_url} alt="" />
-    <div>{props.review.username}</div>
+    <img className="img" src={props.review.image_url} alt="" />
+    <div className="username">{props.review.username}</div>
     {moment(props.review.created_at)
       .startOf("day")
       .fromNow()}
