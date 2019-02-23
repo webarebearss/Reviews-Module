3f8ab408853225e739d2cbc59fe1fdcef1e930f0
Dawn Saquin <dawnsaq@gmail.com>
2019-02-22 22:51:09 -0800

    updated webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,36 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: "babel-loader"
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
       }
     ]
   }
