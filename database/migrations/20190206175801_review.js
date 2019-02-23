12f9923fc6597e9b424e086d4fc5b4ec57309040
Dawn Saquin <dawnsaq@gmail.com>
2019-02-22 20:53:15 -0800

    text schema

--- database/migrations/20190206175801_review.js
+++ database/migrations/20190206175801_review.js
@@ -2,10 +2,10 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable("reviews", function(table) {
       table.increments("review_id").primary();
-      table.string("username");
+      table.text("username");
       table.timestamp("created_at");
-      table.text("description");
-      table.string("image_url");
+      table.text("description", "longtext");
+      table.text("image_url");
       table.integer("user_rating");
       table.integer("accuracy");
       table.integer("communication");
