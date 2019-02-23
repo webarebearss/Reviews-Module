918f40ec82f6a72a68a8a05b3fc9043684512765
Dawn Saquin <dawnsaq@gmail.com>
2019-02-22 21:06:57 -0800

    schema build

--- database/seeds/customer_reviews.js
+++ database/seeds/customer_reviews.js
@@ -1,43 +1,44 @@
 const faker = require("faker");
 
-// const createFakeUser = () => ({
-//   username: faker.name.findName(),
-//   created_at: faker.date.past(),
-//   description: faker.lorem.sentences(),
-//   image_url: faker.image.avatar(),
-//   user_rating: faker.random.number({
-//     min: 0,
-//     max: 100
-//   }),
-//   accuracy: faker.random.number({
-//     min: 3,
-//     max: 5
-//   }),
-//   communication: faker.random.number({
-//     min: 3,
-//     max: 5
-//   }),
-//   cleanliness: faker.random.number({
-//     min: 3,
-//     max: 5
-//   }),
-//   location: faker.random.number({
-//     min: 3,
-//     max: 5
-//   }),
-//   check_in: faker.random.number({
-//     min: 1,
-//     max: 5
-//   }),
-//   value: faker.random.number({
-//     min: 2,
-//     max: 5
-//   }),
-//   listing_id: faker.random.number({
-//     min: 0,
-//     max: 10
-//   })
-// });
+const createFakeUser = () => ({
+  username: faker.name.findName(),
+  created_at: faker.date.past(),
+  description:
+    "here is a review, here is a review, here is a review, here is a review, here is a review, here is a review",
+  image_url: faker.image.avatar(),
+  user_rating: faker.random.number({
+    min: 0,
+    max: 100
+  }),
+  accuracy: faker.random.number({
+    min: 3,
+    max: 5
+  }),
+  communication: faker.random.number({
+    min: 3,
+    max: 5
+  }),
+  cleanliness: faker.random.number({
+    min: 3,
+    max: 5
+  }),
+  location: faker.random.number({
+    min: 3,
+    max: 5
+  }),
+  check_in: faker.random.number({
+    min: 1,
+    max: 5
+  }),
+  value: faker.random.number({
+    min: 2,
+    max: 5
+  }),
+  listing_id: faker.random.number({
+    min: 0,
+    max: 10
+  })
+});
 
 // exports.seed = async function(knex, Promise) {
 //   // each customer review
@@ -49,47 +50,47 @@ const faker = require("faker");
 //   await knex("reviews").insert(fakeUsers);
 // };
 
-// function makeFakeUsers() {
-//   const fakeUsers = [];
-//   const desiredFakeUsers = 100;
-//   for (let i = 0; i < desiredFakeUsers; i++) {
-//     fakeUsers.push(createFakeUser());
-//   }
-//   return fakeUsers;
-// }
+function makeFakeUsers() {
+  const fakeUsers = [];
+  const desiredFakeUsers = 100;
+  for (let i = 0; i < desiredFakeUsers; i++) {
+    fakeUsers.push(createFakeUser());
+  }
+  return fakeUsers;
+}
 
-// exports.seed = function(knex) {
-//   return knex("reviews")
-//     .del()
-//     .then(function() {
-//       return knex("reviews").insert(makeFakeUsers());
-//     });
-// };
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
+exports.seed = function(knex) {
   return knex("reviews")
     .del()
     .then(function() {
-      // Inserts seed entries
-      return knex("reviews").insert([
-        {
-          username: "Missy Elliot",
-          created_at: "2017-04-20 12:29:45.964056",
-          description:
-            "$argon2d$m=4096,t=3,p=1$JGFyZ29uMmQkb$argon2d$m=4096,t=3,p=1$JGFyZ29uMmQkb$argon2d$m=4096,t=3,p=1$JGFyZ29uMmQkb$argon2d$m=4096,t=3,p=1$JGFyZ29uMmQkb",
-          image_url: "$argon2d$m=4096,t=3,p=1$JGFyZ29uMmQkb[...]",
-          user_rating: 6,
-          accuracy: 5,
-          communication: 5,
-          cleanliness: 5,
-          location: 5,
-          check_in: 5,
-          value: 5,
-          listing_id: 5
-        }
-      ]);
+      return knex("reviews").insert(makeFakeUsers());
     });
 };
+
+// exports.seed = function(knex, Promise) {
+//   // Deletes ALL existing entries
+//   return knex("reviews")
+//     .del()
+//     .then(function() {
+//       // Inserts seed entries
+//       return knex("reviews").insert([
+//         {
+//           username: "Missy Elliot",
+//           created_at: "2017-04-20 12:29:45.964056",
+//           description:
+//             "$argon2d$m=4096,t=3,p=1$JGFyZ29uMmQkb$argon2d$m=4096,t=3,p=1$JGFyZ29uMmQkb$argon2d$m=4096,t=3,p=1$JGFyZ29uMmQkb$argon2d$m=4096,t=3,p=1$JGFyZ29uMmQkb",
+//           image_url: "$argon2d$m=4096,t=3,p=1$JGFyZ29uMmQkb[...]",
+//           user_rating: 6,
+//           accuracy: 5,
+//           communication: 5,
+//           cleanliness: 5,
+//           location: 5,
+//           check_in: 5,
+//           value: 5,
+//           listing_id: 5
+//         }
+//       ]);
+//     });
+// };
 
 // faker.lorem.paragraph(nb_sentences=faker.random.number({'min': 1, 'max': 4})),
