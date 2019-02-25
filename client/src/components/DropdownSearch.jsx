e7c84b9f0c04f82882938d15f99ec1b12475aa87
Dawn Saquin <dawnsaq@gmail.com>
2019-02-25 07:52:31 -0800

    added airbnb eslint and modified syntax styling

--- client/src/components/DropdownSearch.jsx
+++ client/src/components/DropdownSearch.jsx
@@ -1,35 +1,36 @@
-import React from "react";
-import Col from "react-bootstrap/Col";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from "react-bootstrap/Dropdown";
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 class DropDownSearch extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      value: "Most recent"
-    };
+    this.state = { value: 'Most recent' };
+    this.change = this.change.bind(this);
   }
 
   change(e) {
+    // const handleValueChange = this.props;
     this.props.handleValueChange(e);
-    e === "recent"
-      ? this.setState({ value: "Most recent" })
-      : this.setState({ value: "Most relevant" });
+    e === 'recent'
+      ? this.setState({ value: 'Most recent' })
+      : this.setState({ value: 'Most relevant' });
   }
 
   render() {
+    const { value } = this.state;
     return (
-      <Col className="dropdown-column" xs sm md lg="3">
+      <Col className='dropdown-column' xs sm md lg='3'>
         <DropdownButton
-          variant="light"
-          id="dropdown-basic-button"
-          title={this.state.value}
-          onSelect={this.change.bind(this)}
+          variant='light'
+          id='dropdown-basic-button'
+          title={value}
+          onSelect={this.change}
         >
-          <Dropdown.Item eventKey="recent">Most recent</Dropdown.Item>
-          <Dropdown.Item eventKey="relevant">Most relevant</Dropdown.Item>
+          <Dropdown.Item eventKey='recent'>Most recent</Dropdown.Item>
+          <Dropdown.Item eventKey='relevant'>Most relevant</Dropdown.Item>
         </DropdownButton>
       </Col>
     );
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,24 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
-import "./stylesheets/style.scss";
+import './stylesheets/style.scss';
 
-import ReviewCount from "./components/ReviewCount.jsx";
-import ConditionsRatings from "./components/ConditionsRatings.jsx";
-import SearchReviews from "./components/SearchReviews.jsx";
-import DropDownSearch from "./components/DropDownSearch.jsx";
-import ReviewList from "./components/ReviewList.jsx";
+import ReviewCount from './components/ReviewCount';
+import ConditionsRatings from './components/ConditionsRatings';
+import SearchReviews from './components/SearchReviews';
+import DropDownSearch from './components/DropdownSearch';
+import ReviewList from './components/ReviewList';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      reviews: []
-    };
+    this.state = { reviews: [] };
     this.setupReviews = this.setupReviews.bind(this);
     this.queryReviewListings = this.queryReviewListings.bind(this);
     this.customReviewListings = this.customReviewListings.bind(this);
@@ -28,100 +26,93 @@ class App extends React.Component {
     this.grabReviews();
   }
 
+  setupReviews(data) {
+    this.setState({ reviews: data });
+  }
+
   async grabReviews() {
     try {
-      const response = await axios.get("/rooms/reviews/recent");
+      const response = await axios.get('/rooms/reviews/recent');
       this.setupReviews(response.data);
     } catch (error) {
       console.error(error);
     }
   }
 
-  setupReviews(data) {
-    this.setState({
-      reviews: data
-    });
-  }
-
   filterReviews(data) {
     if (data.length !== 0) {
-      this.setState({
-        reviews: data
-      });
+      this.setState({ reviews: data });
     }
   }
 
   async queryReviewListings(query) {
     axios
-      .get("/rooms/reviews/filter", {
-        params: { data: query }
-      })
-      .then(res => {
-        this.filterReviews(res.data);
-      });
+      .get('/rooms/reviews/filter', { params: { data: query } })
+      .then(res => this.filterReviews(res.data));
   }
 
   async customReviewListings(query) {
-    axios.get(`/rooms/reviews/${query}`).then(res => {
-      this.filterReviews(res.data);
-    });
+    axios
+      .get(`/rooms/reviews/${query}`)
+      .then(res => this.filterReviews(res.data));
   }
 
   calculateUserRatings(users) {
-    var totalAverage = 0;
-    var ratings = {
+    let totalAverage = 0;
+    const ratings = {
       accuracy: 0,
       communication: 0,
       cleanliness: 0,
       location: 0,
       check_in: 0,
-      value: 0
+      value: 0,
     };
     // grab specific condtions rating from each user
-    for (let i = 0; i < users.length; i++) {
-      ratings["accuracy"] += users[i]["accuracy"];
-      ratings["communication"] += users[i]["communication"];
-      ratings["cleanliness"] += users[i]["cleanliness"];
-      ratings["location"] += users[i]["location"];
-      ratings["check_in"] += users[i]["check_in"];
-      ratings["value"] += users[i]["value"];
+    for (let i = 0; i < users.length; i += 1) {
+      ratings.accuracy += users[i].accuracy;
+      ratings.communication += users[i].communication;
+      ratings.cleanliness += users[i].cleanliness;
+      ratings.location += users[i].location;
+      ratings.check_in += users[i].check_in;
+      ratings.value += users[i].value;
     }
-    for (var key in ratings) {
+    for (const key in ratings) {
       // find the average rating from the users
       ratings[key] = Math.ceil(ratings[key] / users.length);
       totalAverage += ratings[key];
     }
-    ratings["totalAverage"] = Math.ceil(totalAverage / 6);
-    if (ratings["totalAverage"] === NaN) {
-      ratings["totalAverage"] = 0;
+    ratings.totalAverage = Math.ceil(totalAverage / 6);
+    if (ratings.totalAverage === NaN) {
+      ratings.totalAverage = 0;
     }
     return ratings;
   }
 
   render() {
-    var ratings = this.calculateUserRatings(this.state.reviews);
+    const reviews = this.state;
+    const ratings = this.calculateUserRatings(reviews);
 
     return (
-      <Container className="ReviewsContainer">
+      <Container className='ReviewsContainer'>
         <Row>
           <ReviewCount
-            reviewLength={this.state.reviews.length}
-            average={ratings["totalAverage"]}
+            reviewLength={reviews.length}
+            average={ratings.totalAverage}
           />
         </Row>
         <Row>
-          <ConditionsRatings ratings={ratings} reviews={this.state.reviews} />
+          <ConditionsRatings ratings={ratings} reviews={reviews} />
         </Row>
-        <Row className="bottom-spacing top-spacing btn-toolbar">
+        <Row className='bottom-spacing top-spacing btn-toolbar'>
           <SearchReviews handleSearchInput={this.queryReviewListings} />
           <DropDownSearch handleValueChange={this.customReviewListings} />
         </Row>
         <Row>
-          <ReviewList reviews={this.state.reviews} />
+          <ReviewList reviews={reviews} />
         </Row>
       </Container>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("reviews"));
+ReactDOM.render(<App />, document.getElementById('reviews'));
